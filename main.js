8cd49904f48271fe1859ec601ec69a5c8f52e36c
Juanmafa2004 <juanmanuelforero2004@outlook.com>
2023-11-12 23:31:56 -0500

    Se acomodo el menu aside del carrito de compras y se integro con los otros menus despegables

--- main.js
+++ main.js
@@ -3,9 +3,12 @@ const  navbarEmail = $('.navbar-email');
 const desktopMenu = $('.desktop-menu');
 const mobileMenu = $('.mobile-menu') ;
 const bugerMenu = $('.menu');
+const menuCarritoIcon = $(".navbar-shopping-cart");
+const aside = $('.product-detail');
+
 navbarEmail.addEventListener('click', toggleMenu);
 bugerMenu.addEventListener('click', toggleMenu);
-
+menuCarritoIcon.addEventListener('click', toggleCarroAside);
 /* function toggleDesktopMenu(){
 //     desktopMenu.classList.toggle('inactive');
 // }
@@ -16,8 +19,35 @@ bugerMenu.addEventListener('click', toggleMenu);
 
 function toggleMenu(event) {
     if (event.srcElement.className === 'menu') {
+        const isAsideClosed = aside.classList.contains('inactive');
+        if(!isAsideClosed){
+            aside.classList.add('inactive');
+            }
         mobileMenu.classList.toggle('inactive');
-    } else if (event.srcElement.className === 'navbar-email') {
+    } 
+    else if (event.srcElement.className === 'navbar-email') {
+        const isAsideClosed = aside.classList.contains('inactive');
+        if(!isAsideClosed){
+            aside.classList.add('inactive');
+            }
         desktopMenu.classList.toggle('inactive');
     }
 }
+
+function toggleCarroAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    // aside.classList.toggle('inactive');
+     if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+        }
+
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+        }
+
+     aside.classList.toggle('inactive');
+    
+}
+
+
