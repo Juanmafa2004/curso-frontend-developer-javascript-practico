d1717680f79122ef832422e9fabe40192209630f
Juanmafa2004 <juanmanuelforero2004@outlook.com>
2023-11-12 22:41:50 -0500

    Fusión del menú en mobile

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-const  navbarEmail = document.querySelector('.navbar-email');
-const DesktopMenu = document.querySelector('.desktop-menu')
+const $ = (selector) => document.querySelector(selector);
+const  navbarEmail = $('.navbar-email');
+const desktopMenu = $('.desktop-menu');
+const mobileMenu = $('.mobile-menu') ;
+const bugerMenu = $('.menu');
+navbarEmail.addEventListener('click', toggleMenu);
+bugerMenu.addEventListener('click', toggleMenu);
 
-navbarEmail.addEventListener('click', toggleDesktopMenu);
+/* function toggleDesktopMenu(){
+//     desktopMenu.classList.toggle('inactive');
+// }
 
-function toggleDesktopMenu(){
-    DesktopMenu.classList.toggle('inactive');
+// function toggleMobileMenu(){
+
+/ }*/
+
+function toggleMenu(event) {
+    if (event.srcElement.className === 'menu') {
+        mobileMenu.classList.toggle('inactive');
+    } else if (event.srcElement.className === 'navbar-email') {
+        desktopMenu.classList.toggle('inactive');
+    }
 }
